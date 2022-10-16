255532fbf67682235d1fc432a3b2e8a26e6e633c
Jdbmcclintock <43324448+Jdbmcclintock@users.noreply.github.com>
2022-10-17 02:22:11 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,51 @@
 import requests
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import  html
 from dash.dependencies import Input, Output
 import plotly.express as px
 from stats import *
 import pendulum
-
+ 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
-
-
+ 
+ 
 def serve_layout(interval=180000):
     return html.Div([dcc.Interval(id="interval", interval = interval), html.P(id="output")],
                     style = {'width':'100%', 'height':'100vh'})
-
+ 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = serve_layout
-
+ 
 @app.callback(Output("output", "children"), [Input("interval","n_intervals")])
 def dashboard(n):
     last_month = get_last_month()
     json = last_months_json(last_month)
     df = extract_json(json)
     total_tnt = round(df["TNT"].sum())
-
+ 
     converted_tnt, unit = convert_tnt(total_tnt)
-    latest_quake_location, latest_quake_magnitude = get_obspy_text(df.iloc[0, 0])
+    try:
+        latest_quake_location, latest_quake_magnitude = get_obspy_text(df.iloc[0, 0])
+    except:
+        latest_quake_location, latest_quake_magnitude = "ERROR", "ERROR"
     largest_quake_location, largest_quake_magnitude = get_obspy_text(df.query("magnitude == magnitude.max()").iloc[0, 0])
     since_seventyseven = requests.get(
         "http://wfs.geonet.org.nz/geonet/ows?service=WFS&version=1.0.0&request=GetFeature&type" +
         "Name=geonet:quake_search_v1&outputFormat=json&cql_filter=origintime>=2022-05-08T18:00:00").json()
     total_quakes = len(since_seventyseven['features']) + 77000
-
+ 
     return dbc.Container(
         [   dbc.Row(
-
+ 
             dbc.Col(
-                html.H1("NGMC Earthquake Dashboard", style={"fontSize":50, "textAlign":"center"}),
+                html.H1("NGMC Earthquake Dashboard", style={"fontSize":75, "textAlign":"center"}),
                 width = 12, style = {"width": "100%", "height":"100%"}
             ),
         style={"height": "10%"}
         ),
-
+ 
             dbc.Row(
             [
             dbc.Col([
@@ -61,7 +64,7 @@ def dashboard(n):
                                                             'textAlign':'center',
                                                             "marginBottom":0,
                                                             "marginTop":0}),
-
+ 
                         html.P(latest_quake_magnitude + ", " + latest_quake_location, style={"fontSize": 30,
                                                               'textAlign': 'center',
                                                               "marginBottom": 15,
@@ -99,14 +102,15 @@ def dashboard(n):
                                       "marginTop": 0
                                       }),
                         html.P(str(converted_tnt) + unit, style={"fontSize": 30, 'textAlign': 'center'}),
-                        html.P("last run: " + pendulum.now('Pacific/Auckland').to_datetime_string(), style={"fontSize": 10, 'textAlign': 'left'})
+                        html.P("last run: " + pendulum.now('Pacific/Auckland').to_datetime_string(),
+                               style={"fontSize": 15, 'textAlign': 'left', 'marginTop':50})
     ],
                         style={"height  ":"100%"}),
                 style = {"height" : "100%"}
                 ),
-
+ 
                 ]
-
+ 
             ),
             dbc.Col(
                 dcc.Graph(figure=
@@ -125,8 +129,7 @@ def dashboard(n):
         style = {"height" : "90%"}
     )], style={"height": "100vh"}, fluid = True
     )
-
-
+ 
+ 
 if __name__ == '__main__':
-    app.run_server(debug = True, port= 8053)
-
+    app.run_server(debug = True, port= 8060)
